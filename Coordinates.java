c756224e3e94f8651016976dad14d3ae4286abd6
Deepak Singh <39647054+dpk3d@users.noreply.github.com>
2023-01-19 22:20:59 +0530

    Coordinates File

---
+++ Coordinates.java
@@ -0,0 +1,66 @@
+package com.deepak.java.coords;
+
+/**
+ * Encapsulates a point in space with latitude, longitude coordinates.
+ * Can have an error value.
+ *
+ * @author Deepak Singh
+ */
+public class Coordinates {
+    private final Error error;
+    private final LatLonPair latLonPair;
+
+    /**
+     * Create polygon.Coordinates at the specified latitude and longitude.
+     *
+     * @param lat Latitude for this coordinate pair, in degrees.
+     * @param lon Longitude for this coordinate pair, in degrees.
+     */
+    public Coordinates(float lat, float lon) {
+        this.latLonPair = new LatLonPair(lat,lon);
+        this.error = null;
+    }
+
+    /**
+     * todo
+     */
+    public Coordinates(double lat, double lon, double laterr, double lonerr) {
+        this.latLonPair = new coords.LatLonPair(lat,lon);
+        this.error = new Error(laterr, lonerr);
+    }
+
+    /**
+     * Get the latitude of this coordinate pair.
+     *
+     * @return latitude, in degrees.
+     */
+    public double getLatitude() {
+        return latLonPair.getLat();
+    }
+
+    /**
+     * Get the longitude of this coordinate pair.
+     *
+     * @return longitude, in degrees
+     */
+    public double getLongitude() {
+        return latLonPair.getLon();
+    }
+
+    /**
+     * Get the Error values.
+     *
+     * @return Error object
+     */
+    public Error getError() {
+        return error;
+    }
+
+    /**
+     * simple toString
+     */
+    public String toString() {
+        return latLonPair.toString() + " | " + error;
+    }
+
+}
